14596135889c8739a70f9acbf85448cfbd97539d
Christophe Gosiau <christophe@tigron.be>
2019-03-28 12:03:34 +0100

    support reseller users in mysql overview example

--- examples/get_mysql_databases.php
+++ examples/get_mysql_databases.php
@@ -7,32 +7,50 @@ include 'credentials.php';
  * The product_types will be grouped based on their category
  */
 $webhosting = Tigron\CP\Product\Type\Category::get_by_identifier('webhosting');
-$products = Tigron\CP\Product::get_by_user_category(Tigron\CP\User::get(), $webhosting);
+$user = \Tigron\CP\User::Get();
 
-foreach ($products as $product) {
-	$product_type = \Tigron\CP\Product\Type::get_by_id($product->product_type_id);
-	echo $product->domain . '.' . $product->tld . " (" . $product_type->name . ")\n";
+if ($user->is_reseller) {
+	$reseller = \Tigron\CP\Reseller::get_by_id($user->reseller_id);
+	$users = \Tigron\CP\User::get_by_reseller($reseller);
+} else {
+	$users = [ $user ];
+}
 
-	try {
-		$mysql_product = $product->get_mysql_product();
-	} catch (Exception $e) {
-		echo "\t" . 'no databases' . "\n";
+
+foreach ($users as $user) {
+
+	$products = Tigron\CP\Product::get_by_user_category($user, $webhosting);
+
+	if (count($products) == 0) {
 		continue;
 	}
 
-	$databases = \Tigron\CP\Mysql::get_by_product($mysql_product);
+	echo $user->username . "\n";
 
-	if (count($databases) == 0) {
-		echo "\t" . 'no databases' . "\n";
-		continue;
-	}
+	foreach ($products as $product) {
+		$product_type = \Tigron\CP\Product\Type::get_by_id($product->product_type_id);
+		echo "\t" . $product->domain . '.' . $product->tld . " (" . $product_type->name . ")\n";
 
-	foreach ($databases as $database) {
-		$external_connections = 'false';
-		if ($database->host != 'localhost') {
-			$external_connections = 'true';
+		try {
+			$mysql_product = $product->get_mysql_product();
+		} catch (Exception $e) {
+			echo "\t\t" . 'no databases' . "\n";
+			continue;
 		}
-		echo "\t" . 'Database: "' . $database->database_name . '", Username: "' . $database->username . '", External connections: ' . $external_connections . "\n";
-	}
 
+		$databases = \Tigron\CP\Mysql::get_by_product($mysql_product);
+
+		if (count($databases) == 0) {
+			echo "\t\t" . 'no databases' . "\n";
+			continue;
+		}
+
+		foreach ($databases as $database) {
+			$external_connections = 'false';
+			if ($database->host != 'localhost') {
+				$external_connections = 'true';
+			}
+			echo "\t\t" . 'Database: "' . $database->database_name . '", Username: "' . $database->username . '", External connections: ' . $external_connections . "\n";
+		}
+	}
 }
